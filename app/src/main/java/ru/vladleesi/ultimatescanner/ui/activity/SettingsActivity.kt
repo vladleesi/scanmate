bc6427b0a18bb8ff733eaf9f42d60f96a0a17295
kvv <blacoll11@gmail.com>
2021-08-07 23:20:02 +0300

    updated settings screen

--- app/src/main/java/ru/vladleesi/ultimatescanner/ui/activity/SettingsActivity.kt
+++ app/src/main/java/ru/vladleesi/ultimatescanner/ui/activity/SettingsActivity.kt
@@ -1,59 +1,42 @@
 package ru.vladleesi.ultimatescanner.ui.activity
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import ru.vladleesi.ultimatescanner.R
-import ru.vladleesi.ultimatescanner.data.repository.AnalyzeRepo
 import ru.vladleesi.ultimatescanner.databinding.ActivitySettingsBinding
 import ru.vladleesi.ultimatescanner.ui.fragments.SettingsPreferenceFragment
-import java.lang.ref.WeakReference
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivitySettingsBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tbToolbar.navigationIcon =
+        initToolbar()
+
+        if (savedInstanceState == null) openSettingsFragment()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.includeToolbar.tbToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.includeToolbar.tbToolbar.navigationIcon =
             ContextCompat.getDrawable(baseContext, R.drawable.ic_baseline_arrow_back_24)
-        binding.tbToolbar.setNavigationOnClickListener { onBackPressed() }
-        binding.tbToolbar.title = "Настройки"
+        binding.includeToolbar.tbToolbar.setNavigationOnClickListener { onBackPressed() }
+        binding.includeToolbar.tbToolbar.title = getString(R.string.tv_settings)
+    }
 
-        if (savedInstanceState == null)
-            supportFragmentManager.beginTransaction()
-                .add(
-                    binding.fcvMainContainer.id,
-                    SettingsPreferenceFragment.newInstance(),
-                    SettingsPreferenceFragment.TAG
-                )
-                .commit()
-
-        val handler = CoroutineExceptionHandler { _, throwable ->
-            lifecycleScope.launch {
-                Toast.makeText(
-                    baseContext,
-                    "ERROR: ${throwable.message ?: throwable.toString()}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-
-        binding.mbTestConnection.setOnClickListener {
-
-            GlobalScope.launch(handler) {
-                val message = AnalyzeRepo(WeakReference(applicationContext)).testConnectionAsync()
-                lifecycleScope.launch {
-                    Toast.makeText(baseContext, message, Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+    private fun openSettingsFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(
+                binding.fcvMainContainer.id,
+                SettingsPreferenceFragment.newInstance(),
+                SettingsPreferenceFragment.TAG
+            )
+            .commit()
     }
 }
--- app/src/main/java/ru/vladleesi/ultimatescanner/ui/fragments/SettingsPreferenceFragment.kt
+++ app/src/main/java/ru/vladleesi/ultimatescanner/ui/fragments/SettingsPreferenceFragment.kt
@@ -1,13 +1,50 @@
 package ru.vladleesi.ultimatescanner.ui.fragments
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import ru.vladleesi.ultimatescanner.R
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import ru.vladleesi.ultimatescanner.data.repository.AnalyzeRepo
+import ru.vladleesi.ultimatescanner.extensions.showToast
+import java.lang.ref.WeakReference
 
 class SettingsPreferenceFragment : PreferenceFragmentCompat() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        disableOverScroll()
+    }
+
+    private fun disableOverScroll() {
+        listView.overScrollMode = View.OVER_SCROLL_NEVER
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.prefs, rootKey)
+        setPreferencesFromResource(ru.vladleesi.ultimatescanner.R.xml.prefs, rootKey)
+        initTestConnectionButton()
+    }
+
+    private fun initTestConnectionButton() {
+        val handler = CoroutineExceptionHandler { _, throwable ->
+            lifecycleScope.launch {
+                activity?.showToast("ERROR: ${throwable.message ?: throwable.toString()}")
+            }
+        }
+
+        findPreference<Preference>("test_connection")?.onPreferenceClickListener =
+            Preference.OnPreferenceClickListener {
+                GlobalScope.launch(handler) {
+                    val message = AnalyzeRepo(WeakReference(context)).testConnection()
+                    lifecycleScope.launch {
+                        activity?.showToast(message)
+                    }
+                }
+                true
+            }
     }
 
     companion object {
